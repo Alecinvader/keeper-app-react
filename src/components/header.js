fefc6f51b3eb740664c2a00836d28d2dd6f65fbf
alec-r-reynolds <alechasrito@gmail.com>
2020-08-04 22:27:35 -0600

    Project setup

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+
+export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  
+  render() {
+      return (
+        <header>
+          <h1>Keeper</h1>
+        </header>
+      );
+  }
+}
+
